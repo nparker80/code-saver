ee5315e21918ffbda8c0e2086032f7c631d4816c
Najla Parker <najla08@gmail.com>
2022-04-03 14:36:22 -0600

    Troubleshooting manifest and sw.js issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -27,8 +25,8 @@ module.exports = () => {
         inject: true,
         name: "JATE",
         description: "Just another text editor",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
+        background_color: "#225CA3",
+        theme_color: "#225CA3",
         start_url: "/",
         publicPath: "/",
         icons: [
@@ -44,7 +42,6 @@ module.exports = () => {
         swDest: "src-sw.js",
       }),
     ],
-
     module: {
       rules: [
         {
